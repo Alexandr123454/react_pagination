26085dce8d60ef83fff3feed6921f9cf84f95943
alexandr <alexandrov6927@gmail.com>
2019-08-06 13:03:22 +0300

    Updates

---
+++ precache-manifest.1c56b6e3676aec8aed6c7c38b0c1c32d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed471ad967735a80b070f2de5e23c805",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "6de208c5bb1caa98bb28",
+    "url": "/react_pagination/static/css/main.d10381c2.chunk.css"
+  },
+  {
+    "revision": "906b89fbd6efa5f6d981",
+    "url": "/react_pagination/static/js/2.eb365f0c.chunk.js"
+  },
+  {
+    "revision": "6de208c5bb1caa98bb28",
+    "url": "/react_pagination/static/js/main.a8e71255.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
